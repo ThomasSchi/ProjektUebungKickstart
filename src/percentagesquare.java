d311a3ca2bfb7eba549ae2ade1f3fd1f2806e558
nadine <nadine.blaas@accenture.com>
2023-01-13 14:06:49 +0100

    Message

--- src/percentagesquare.java
+++ src/percentagesquare.java
@@ -2,32 +2,38 @@
 public class percentagesquare {
 
     public static void percentage(double x, double y) {
+        //Berechnung des Prozentanteils von x zu y
         double ergebnis = (x * 100) / y;
+        //Gibt Prozentanteil auf Konsole aus
         System.out.println(x + " von " + y + " = " + ergebnis + "%");
     }
 
-    public static void squareroot(double x) {
-        double ergebnis = Math.sqrt(x);
-        System.out.println("Die Wurzel von " + x + " = " + ergebnis);
-    }
 
     public static void addition(double x, double y) {
+        //Berechnung der Addition von x und y
         double ergebnis = x + y;
+        //Gibt Ergebnis der Addition auf Konsole aus
         System.out.println(x + " plus " + y + " = " + ergebnis);
         }
 
     public static void subtraktion(double x, double y) {
+        //Berechnung der Subtraktion von x und y
         double ergebnis = x - y;
+        //Gibt Ergebnis der Subtraktion auf Konsole aus
         System.out.println(x + " minus " + y + " = " + ergebnis);
         }
 
         public static void multiplikation( double x, double y){
+            //Berechnung der Multiplikation von x und y
             double ergebnis = x * y;
+            //Gibt Ergebnis der Multiplikation auf Konsole aus
             System.out.println(x + " mal " + y + " = " + ergebnis);
         }
 
         public static void division( double x, double y){
+            //Berechnung der Division von x und y
             double ergebnis = x / y;
+            //Gibt Ergebnis der Division auf Konsole aus
             System.out.println(x + " geteilt durch " + y + " = " + ergebnis);
         }
     }
