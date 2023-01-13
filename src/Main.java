ae9489f62a75d291c83aa54d94eb2e41d874e8df
ThomasSchi <thomasschilinski@yahoo.de>
2023-01-13 10:26:54 +0100

    änderung von thomas

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hello group");
+
+        int i = 6;
+                int p = 7;
+
+                if (i == 6) {
+                    System.out.println("hallo amira");
+                }else{
+                    System.out.println( "hallo nadine");
+                }
         System.out.println(123);
     }
 }
