ff70247e4f1e04b7bbc182cbca173ceebc49969f
khoi.hoang <khoi.hoang@accenture.com>
2023-01-13 13:06:19 +0100

    multiplikation und division hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+    }
 
     public static void mal(double x, double y){
         double ergebnis = x * y;
