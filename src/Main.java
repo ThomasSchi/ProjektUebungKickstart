fc88513a1f9979dd57c37f9ef024237bbcdc3e3a
amiracon <contichamira@gmail.com>
2023-01-13 10:26:35 +0100

    Hello

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int age = 30;
 
         System.out.println("hello group");
+        // Lass uns jetzt ein If else programmieren hello
     }
+
 }
