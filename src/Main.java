de482c93b1b00a18997d139ef1e8937aee67a650
khoi.hoang <khoi.hoang@accenture.com>
2023-01-13 13:02:10 +0100

    multiplikation und division hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        addieren(s);
 
     }
 
-    public static void addieren(Scanner plus) {
-        System.out.println("zahl zum addieren eingeben = ");
-        int zahl1;
-        zahl1 = plus.nextInt();
-        System.out.println("zahl1=" + zahl1);
-
-        System.out.println("zahl zum addieren eingeben = ");
-        int zahl2;
-        zahl2 = plus.nextInt();
-        System.out.println("zahl2=" + zahl2);
-
-        int summe = zahl1 + zahl2;
-
-        System.out.println("summe=" + summe);
-
+    public static void mal(double x, double y){
+        double ergebnis = x * y;
+        System.out.println(x+" * "+y+ " = "+ ergebnis);
+    }
+    public static void geteilt(double x, double y){
+        double ergebnis = x/y;
+        System.out.println(x+" / "+" = "+ergebnis);
     }
 }
