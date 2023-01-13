ade3b7a3b2df9c37347db1cd586e858631308d3d
nadine <nadine.blaas@accenture.com>
2023-01-13 13:07:45 +0100

    initialize git

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    }
 
-    public static void mal(double x, double y){
-        double ergebnis = x * y;
-        System.out.println(x+" * "+y+ " = "+ ergebnis);
-    }
-    public static void geteilt(double x, double y){
-        double ergebnis = x/y;
-        System.out.println(x+" / "+" = "+ergebnis);
+
     }
 }
-
 
 
 
--- src/percentagesquare.java
+++ src/percentagesquare.java
@@ -3,24 +3,31 @@ public class percentagesquare {
 
     public static void percentage(double x, double y) {
 
-            double ergebnis = (x*100)/y;
-            System.out.println(x + " von " + y + " = " + ergebnis +"%");
+        double ergebnis = (x * 100) / y;
+        System.out.println(x + " von " + y + " = " + ergebnis + "%");
     }
 
     public static void squareroot(double x) {
-            double ergebnis = Math.sqrt(x);
-        System.out.println("Die Wurzel von " + x + " = " + ergebnis );
+        double ergebnis = Math.sqrt(x);
+        System.out.println("Die Wurzel von " + x + " = " + ergebnis);
 
     }
 
-    public static double summe ( double x, double y){
+    public static double summe(double x, double y) {
         return x + y;
 
     }
 
-    public static double subtraktion ( double x, double y){
+    public static double subtraktion(double x, double y) {
         return x - y;
 
-
+        public static void mal ( double x, double y){
+            double ergebnis = x * y;
+            System.out.println(x + " * " + y + " = " + ergebnis);
+        }
+        public static void geteilt ( double x, double y){
+            double ergebnis = x / y;
+            System.out.println(x + " / " + " = " + ergebnis);
+        }
     }
 }
