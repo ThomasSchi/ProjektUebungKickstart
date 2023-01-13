9e23d01583f0f3cfdc0bd73cf7ebba43347d8f72
amiracon <contichamira@gmail.com>
2023-01-13 14:02:40 +0100

    Fertig

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,60 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        // Scanner erstellt und benannt
         Scanner eingabe = new Scanner(System.in);
-        double x,y;
+        // Double-Variablen X und Y  initialisiert
+        double x, y;
+        // String Operator initialisiert
         String operator;
 
-        System.out.print("Bitte geben Sie Ihre erste Zahl ein: ");
-        x=eingabe.nextDouble();
 
+        // Printed "Bitte geben Sie Ihre erste Zahl ein: " auf die Konsole
+        System.out.print("Bitte geben Sie Ihre erste Zahl ein: ");
+        x = eingabe.nextDouble();
+
+        // Printed "Bitte geben Sie Ihren Operator ein: " auf die Konsole
         System.out.print("Bitte geben Sie Ihren Operator ein: ");
-        operator=eingabe.next();
+        operator = eingabe.next();
+        //// Printed "Bitte geben Sie Ihre zweite Zahl ein: "auf die Konsole
 
         System.out.print("Bitte geben Sie Ihre zweite Zahl ein: ");
-        y=eingabe.nextDouble();
+            y = eingabe.nextDouble();
 
-        eingabe.close();
+            eingabe.close();
 
-        double result = 0;
+            double result = 0;
 
-        switch(operator){
-            case "+":
-                percentagesquare.addition(x,y);
-                break;
-            case "-":
-                percentagesquare.subtraktion(x,y);
-                break;
-            case"*":
-                percentagesquare.multiplikation(x,y);
-                break;
-            case"/":
-                percentagesquare.division(x,y);
-                break;
-            case"w":
-                percentagesquare.squareroot(x);
-            case"p":
-                percentagesquare.percentage(x,y);
-            default:
-                System.out.print("Falscher Operator. Bitte Neueingabe.");
-                return;
+            switch (operator) {
+                case "+":
+                    // Ruft die Methode Addition auf
+                    percentagesquare.addition(x, y);
+                    break;
+                case "-":
+                    // Ruft die Methode Subtraktion auf
+                    percentagesquare.subtraktion(x, y);
+                    break;
+                case "*":
+                    // Ruft die Methode Multiplikation auf
+                    percentagesquare.multiplikation(x, y);
+                    break;
+                case "/":
+                    // Ruft die Methode Division auf
+                    percentagesquare.division(x, y);
+                    break;
+                case "p":
+                    // Ruft die Methode Percentage auf
+                    percentagesquare.percentage(x, y);
+                    break;
+                default:
+                    // Bei falscher Eingabe Printed "Falscher Operator. Bitte Neueingabe." auf die Konsole
+                    System.out.print("Falscher Operator. Bitte Neueingabe.");
+                    return;
 
             }
 
         }
     }
+
 
 
