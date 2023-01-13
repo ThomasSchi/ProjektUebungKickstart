4ba79ed76eba20152ab827ccf66b842283d20d66
nadine <nadine.blaas@accenture.com>
2023-01-13 11:30:35 +0100

    initialize git

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        addieren(s);
 
-    }
+        double x = 5;
 
-    public static void addieren(Scanner plus) {
-        System.out.println("zahl zum addieren eingeben = ");
-        int zahl1;
-        zahl1 = plus.nextInt();
-        System.out.println("zahl1=" + zahl1);
-
-        System.out.println("zahl zum addieren eingeben = ");
-        int zahl2;
-        zahl2 = plus.nextInt();
-        System.out.println("zahl2=" + zahl2);
-
-        int summe = zahl1 + zahl2;
-
-        System.out.println("summe=" + summe);
+        percentagesquare.squareroot(x);
 
     }
 }
